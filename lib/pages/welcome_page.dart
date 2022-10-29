67feb7c6738e7dcd4f04d94ffc6081c10f51c70a
Fatahillah_Abid <81362950+Fatahillah-13@users.noreply.github.com>
2022-10-29 07:38:35 +0700

    First Commit

---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class welcomePage extends StatefulWidget {
+  const welcomePage({super.key});
+
+  @override
+  State<welcomePage> createState() => _welcomePageState();
+}
+
+class _welcomePageState extends State<welcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: 3,
+        itemBuilder: (_, index) {
+          return Container(
+            width: double.maxFinite,
+            height: double.maxFinite,
+          );
+        },
+      ),
+    );
+  }
+}

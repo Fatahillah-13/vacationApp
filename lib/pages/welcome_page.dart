734d134e22da5ce5b68d3e0ae28f57851af219a4
Fatahillah_Abid <81362950+Fatahillah-13@users.noreply.github.com>
2022-10-29 09:47:27 +0700

    Make text function and Appcolor function

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,24 +1,71 @@
+// ignore_for_file: prefer_interpolation_to_compose_strings
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:vacation_app/misc/colors.dart';
+import 'package:vacation_app/widgets/app_large_text.dart';
+import 'package:vacation_app/widgets/app_text.dart';
 
-class welcomePage extends StatefulWidget {
-  const welcomePage({super.key});
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
 
   @override
-  State<welcomePage> createState() => _welcomePageState();
+  State<WelcomePage> createState() => _WelcomePageState();
 }
 
-class _welcomePageState extends State<welcomePage> {
+class _WelcomePageState extends State<WelcomePage> {
+  List images = [
+    "welcome-one.png",
+    "welcome-two.png",
+    "welcome-three.png",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView.builder(
         scrollDirection: Axis.vertical,
-        itemCount: 3,
+        itemCount: images.length,
         itemBuilder: (_, index) {
           return Container(
             width: double.maxFinite,
             height: double.maxFinite,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  "assets/img/" + images[index],
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Container(
+              margin: const EdgeInsets.only(
+                top: 150,
+                left: 20,
+                right: 20,
+              ),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      AppLargeText(text: "Trips"),
+                      AppText(
+                        text: "Mountain",
+                        size: 30,
+                      ),
+                      Container(
+                        width: 250,
+                        child: AppText(
+                          text:
+                              "Mountain hikes give you an incredible sense of freedom along with endurance test.",
+                          color: AppColors.textColor2,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
           );
         },
       ),
